eaeabf82d9431153ceab507cb2bee01bae5ed685
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-07-06 09:21:03 +0000

    Make the key expiry date display when click check key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 const API_KEY = 'qKvNJCu_s9H-RPGgUz6bF_RLMDg';
-const API_URL = 'https://ci-jshint.herokuapp.com/api?api'
+const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+
+document.getElementById('status').addEventListener('click', e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error)
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerHTML = ('API Key Status')
+    document.getElementById("results-content").innerHTML = (`Your Key is valid until: <strong>${data.expiry}<strong/>`)
+
+
+
+    resultsModal.show();
+}
