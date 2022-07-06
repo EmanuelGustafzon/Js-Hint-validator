548c7c6dba182f861b6d8501ee22ea7a4c3c387b
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-07-06 08:00:20 +0000

    Add API to the project

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = 'qKvNJCu_s9H-RPGgUz6bF_RLMDg';
+const API_URL = 'https://ci-jshint.herokuapp.com/api?api'
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
